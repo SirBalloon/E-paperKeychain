861ead4b81a4d4f5f954b80ea0372b3cda9ffb2a
SirBalloon <kibrahim17@hotmail.com>
2026-05-11 20:44:55 +1000

    Updated comments on the old E-Paper Code

--- E-PaperServer.ino
+++ E-PaperServer.ino
@@ -9,30 +9,34 @@
 #include <AsyncTCP.h>
 
 // Pin definitions
-#define EPD_MOSI  11
-#define EPD_SCK   12
-#define EPD_CS    10
-#define EPD_DC    9
-#define EPD_RST   8
-#define EPD_BUSY  7
+#define EPD_MOSI 11
+#define EPD_SCK 12
+#define EPD_CS 10
+#define EPD_DC 9
+#define EPD_RST 8
+#define EPD_BUSY 7
 
-#define DISPLAY_WIDTH  296
+#define DISPLAY_WIDTH 296
 #define DISPLAY_HEIGHT 128
 
-// 2.9'' EPD Module
+// 2.9'' EPD Module - Considered a Template Class
 GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> display(GxEPD2_290_C90c(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY)); // GDEM029C90 128x296, SSD1680
 
-const char* ssid = "HoneuuuCinnamonRoll";
-const char* password = "Password1234";
+// Webserver credentials
+const char *ssid = "HoneuuuCinnamonRoll";
+const char *password = "Password1234";
 
+// Sets up the Server on port 80
 AsyncWebServer server(80);
 
-// Buffer to store uploaded image data
+// Buffer to store uploaded image data. Each pixel is 1 bit, we can pack 8 bits into a byte so the image (296*128 = 37,888)/8 = 4736 bytes.
+// Which is much more efficient then storing each pixel as a full byte or 4-byte RGBA Value
 const size_t IMAGE_BUFFER_SIZE = (DISPLAY_WIDTH * DISPLAY_HEIGHT) / 8; // 4,736 bytes
-uint8_t* imageBuffer = nullptr;
+uint8_t *imageBuffer = nullptr;
 size_t totalReceived = 0;
 bool imageReadyToDisplay = false;
 
+// Using PROGMEM to save the HTML server code that will run on port 80
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
 <html>
@@ -407,50 +411,61 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
-void displayUploadedImage(uint8_t* data, size_t len) {
+// This Function uploads the image onto the e-paper display
+void displayUploadedImage(uint8_t *data, size_t len)
+{
   Serial.println("Displaying image on e-paper...");
-  
-  if (len != IMAGE_BUFFER_SIZE) {
+
+  if (len != IMAGE_BUFFER_SIZE)
+  {
     Serial.printf("Error: Expected %d bytes, got %d bytes\n", IMAGE_BUFFER_SIZE, len);
     return;
   }
-  
+
   display.setFullWindow();
   display.firstPage();
-  
-  do {
+
+  do
+  {
     display.fillScreen(GxEPD_WHITE);
-    
-    for (int y = 0; y < DISPLAY_HEIGHT; y++) {
-      for (int x = 0; x < DISPLAY_WIDTH; x++) {
+
+    for (int y = 0; y < DISPLAY_HEIGHT; y++)
+    {
+      for (int x = 0; x < DISPLAY_WIDTH; x++)
+      {
         int byteIdx = y * ((DISPLAY_WIDTH + 7) / 8) + (x / 8);
         int bitIdx = 7 - (x % 8);
-        
-        if (data[byteIdx] & (1 << bitIdx)) {
+
+        if (data[byteIdx] & (1 << bitIdx))
+        {
           display.drawPixel(x, y, GxEPD_BLACK);
         }
       }
-      
-      if (y % 10 == 0) {
+
+      if (y % 10 == 0)
+      {
         yield();
       }
     }
   } while (display.nextPage());
-  
+
   Serial.println("Image displayed successfully!");
 }
 
-void setup() {
+// This Function sets up the initial E-paper display allowing the user experience to start
+void setup()
+{
   Serial.begin(115200);
   Serial.println("\n=== Project E-paper Gift ===");
-  
+
   SPI.begin(EPD_SCK, -1, EPD_MOSI, EPD_CS);
-  
+
   display.init(115200);
   display.setRotation(1);
 
-  imageBuffer = (uint8_t*)malloc(IMAGE_BUFFER_SIZE);
-  if (imageBuffer == nullptr) {
+  imageBuffer = (uint8_t *)malloc(IMAGE_BUFFER_SIZE);
+  if (imageBuffer == nullptr)
+  {
     Serial.println("Failed to allocate image buffer!");
   }
 
@@ -463,16 +478,12 @@ void setup() {
   Serial.println(ssid);
   Serial.println("Then visit: http://192.168.4.1");
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", index_html);
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/html", index_html); });
 
-  server.on("/upload", HTTP_POST, 
-    [](AsyncWebServerRequest *request) {
-      request->send(200, "text/plain", "OK");
-    }, 
-    NULL, 
-    [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+  server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "OK"); }, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
+            {
       if (index == 0) {
         totalReceived = 0;
         Serial.printf("Upload started, expecting %d bytes\n", total);
@@ -490,16 +501,15 @@ void setup() {
         if (totalReceived == IMAGE_BUFFER_SIZE) {
           imageReadyToDisplay = true;
         }
-      }
-    }
-  );
+      } });
 
   server.begin();
 
   Serial.println("Updating display...");
   display.setFullWindow();
   display.firstPage();
-  do {
+  do
+  {
     display.fillScreen(GxEPD_WHITE);
     display.setTextColor(GxEPD_BLACK);
     display.setFont(&FreeMonoBold12pt7b);
@@ -510,12 +520,15 @@ void setup() {
     display.setCursor(10, 90);
     display.println("192.168.4.1");
   } while (display.nextPage());
-  
+
   Serial.println("Setup complete!");
 }
 
-void loop() {
-  if (imageReadyToDisplay) {
+// This is the server loop which looks for if there are images to display
+void loop()
+{
+  if (imageReadyToDisplay)
+  {
     imageReadyToDisplay = false;
     displayUploadedImage(imageBuffer, totalReceived);
   }
